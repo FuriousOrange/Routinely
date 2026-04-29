c0b60d908ed38ec8e518d52fa6cb0c3a3cf22e2e
James Herring <james794251@hotmail.com>
2026-04-29 15:20:52 +0100

    Update test base to include wait value on manuel reset event

--- Routinely.UnitTests/CoroutineTestBase.cs
+++ Routinely.UnitTests/CoroutineTestBase.cs
@@ -83,7 +83,7 @@ public abstract class CoroutineThreadTestBase : CoroutineTestBase
 
         startEvent.Set();
 
-        stopEvent.WaitHandle.WaitOne();
+        stopEvent.WaitHandle.WaitOne(5000);
 
         var exceptions = exceptionQueue.ToList();
 
